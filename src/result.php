efd4162cbad55082197f614c6ac6ed399bc505f0
Alexander Lapod <alexanderfabian18@gmail.com>
2026-01-17 16:43:22 +0700

    formatting: path

--- src/result.php
+++ src/result.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 use Phpml\FeatureExtraction\TokenCountVectorizer;
 use Phpml\Tokenization\WhitespaceTokenizer;
 use Phpml\FeatureExtraction\TfIdfTransformer;
